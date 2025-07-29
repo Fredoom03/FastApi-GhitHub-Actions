d1f278676369d5c6c0280d922f2c4693ab09aa06
Fredoom03 <fredy_luque@hotmail.com>
2025-07-29 05:09:20 +0000

    Fix linting

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
 from app.deps import create_database_and_tables
 from app.routes import user
 
-
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
-import uvicorn
-
-
+from fastapi import FastAPI
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_database_and_tables()
     yield
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(user.router)
@@ -20,13 +17,3 @@ app.include_router(user.router)
 @app.get("/health")
 async def service_health():
     return {"status": "healhty"}
-
-
-# def main():
-#     create_database_and_tables()
-#     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
-
-
-# if __name__ == "__main__":
-#     create_database_and_tables()
-#     main()
